acf47836c3f107713791e70b2dd1cb5d62a72717
opilane <opilane@HP-CZC2349HT4>
2023-03-07 15:08:17 +0200

    Add project files.

---
+++ MyModule.py
@@ -0,0 +1,70 @@
+﻿from класс1 import*
+
+text_est = loe_failist('est.txt')
+text_rus = loe_failist('rus.txt')
+
+
+while True:
+           print("1-Перевод с эстонского языка на русский")
+           print("2-Перевод с русского языка на эстонский")
+           print("3-Добавте слово в словарь")
+           print("4-Можете исправить ошибку в слове")
+           print("5-Проверка знания слов")
+           print("6-Закончить работу")
+           v=int(input("Выберите действие: "))
+           if v==1:
+                  print()
+                  print("Перевод с эстонского языка на русский")
+                  print()
+                  text_es = input("Введите слово на эстонском языке: ")
+                  text_ru = text_est_v_text_rus(text_es, text_est, text_rus)
+                  if text_ru:
+                        print("В переводе на русский это", text_ru)
+                  else:
+                        print("Ошибка")
+
+           elif v==2:
+                    print()
+                    print("Перевод с русского языка на эстонский")
+                    print()
+                    text_ru = input("Введите слово на русском языке: ")
+                    text_es = rus_na_est(text_ru, text_est, text_rus)
+                    if text_es:
+                        print("В переводе на эстонский это", text_es)
+                    else:
+                        print("Ошибка")
+
+           elif v==3:
+                    print()
+                    print("Добавте слово в словарь")
+                    print()
+                    n=int(input("Сколько слов вы хотите добавить: "))
+                    for i in range(n):
+                        text=input("Напишите слово на эстонском: ")
+                        text_ap(text, text_est, text_rus)
+                        print()
+                    laused=loe_failist("est.txt")
+                    for line in laused:
+                        print(line)
+                    print()    
+                    laused=loe_failist("rus.txt")
+                    for line in laused:
+                        print(line)
+                    print()
+
+           elif v==4:
+                    print()
+                    print("Можете исправить ошибку в слове")
+                    print()
+                    correct_word(text_est, text_rus, text)
+                    
+                    laused=loe_failist("est.txt")
+                    for line in laused:
+                        print(line)
+
+           elif v==5:
+                print()
+                spin2=teadmiste_kontroll(text_rus,text_est)
+           else:
+               print("Конец")
+               break
---
+++ rabota.py
@@ -0,0 +1,107 @@
+﻿import random 
+
+def loe_failist(fail:str)->list:
+
+    f=open(fail,"r",encoding="utf-8-sig")
+    jarjend=[]
+    for rida in f:
+        jarjend.append(rida.strip())
+    f.close()
+    return jarjend
+
+def kirjuta_failist(fail:str,jarjend:list):
+    f=open(fail,"w",encoding="utf-8-sig")
+    for line in jarjend:
+        f.write(line+"\n")
+    f.close
+
+
+    
+def text_est_v_text_rus(text:str, text_est:list, text_rus:list):
+    if text in text_est:
+        ind=text_est.index(text)
+
+    return text_rus[ind]
+ 
+
+def rus_na_est(text:str, text_est:list,  text_rus:list ):
+    if text in  text_rus:
+        ind =  text_rus.index(text)
+        
+    return text_est[ind]
+  
+
+def text_ap(text:str, text_est:list, text_rus:list):
+   
+
+        if text not in  text_est:
+            text_est.append(text)
+            print()
+        j=input("Обозначение этого слова на русском: ")
+        text_rus.append(j)
+        kirjuta_failist('est.txt', text_est)
+        kirjuta_failist('rus.txt', text_rus)
+        return  j
+
+
+def correct_word(text:str,text_est:list,text_rus:list):
+
+    if text in text_rus:
+       index = text_rus.index(text)
+       text = input("Введите слово для исправления: ")
+       new_translation = input(f"Введите новое значение для слова '{text}': ")
+       text_est[index] = new_translation
+       print("Слово успешно исправлено!")
+    
+    elif text in text_est:
+         index=text_est.index(text)
+         text = input("Введите слово для исправления: ")
+         new_translation = input(f"Введите новое значение для слова '{text}': ")
+         text_rus[index] = new_translation
+         print("Слово успешно исправлено!")
+
+    else:
+        print("Такого слова нету в словаре")
+
+    
+
+
+
+def teadmiste_kontroll(text_rus:list,text_est:list):
+
+    p=0
+    kokku=int(input("Mitu küsimust? "))
+    for i in range(kokku):
+        järjend=random.choice([text_rus,text_est])
+        sõna=random.choice(järjend)
+        print(f"{sõna} - ",end="")
+        tõlke=input()
+        if sõna in text_rus:
+           i=text_rus.index(sõna)
+           tõlke_kontroll=text_est[i]
+        elif sõna in text_est:
+            i=text_est.index(sõna)
+            tõlke_kontroll=text_rus[i]
+        if tõlke==tõlke_kontroll:
+            p+=1
+        if (p/kokku)*100>90:
+            hinne=5
+        elif (p/kokku)*100>75:
+            hinne=4
+        elif (p/kokku)*100>60:
+            hinne=3
+        else:
+            hinne="Väga halb"
+            print("Õige")
+            return hinne
+    else:
+         print("Vale")
+    if (p/kokku)*100>90:
+        hinne=5
+    elif (p/kokku)*100>75:
+        hinne=4
+    elif (p/kokku)*100>60:
+        hinne=3
+    else:
+         hinne="Väga halb"
+    return hinne
